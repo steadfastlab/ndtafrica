96b005557c88024fc2f13b5c8f817cabda92b1a2
steadfastlab <156627775+steadfastlab@users.noreply.github.com>
2024-02-13 13:55:18 +0000

    Add files via upload
    ndt africa save you from loss run to us good mechanic works

---
+++ script.js
@@ -0,0 +1,18 @@
+const navSlide = () => {
+    const navButton = document.querySelector('.headerNavMenu');
+    const navList = document.querySelector('.headerNavList');
+    const navLink = document.querySelectorAll('.headerNavList li');
+    navButton.addEventListener('click', () => {
+        navList.classList.toggle('headerNavList-active');
+        navLink.forEach((link ,index) => {
+            if(link.style.animation) {
+                link.style.animation = "";
+            } else {
+                link.style.animation = `navLink 0.5s ease forwards ${ index / 7 + 0.3}s`;
+            }
+        });
+        navButton.classList.toggle('toggle');
+    });
+
+}    
+navSlide();
